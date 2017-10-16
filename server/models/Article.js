e98fe72c8830d84b08ba59a4383420aebccddadc
wangyu <13022200357@163.com>
2017-10-16 14:50:06 +0800

    [后端]: 更新article表字段

--- server/models/Article.js
+++ server/models/Article.js
@@ -4,8 +4,8 @@ module.exports = db.defineModel('article', {
   title: db.STRING(100),
   cover: db.STRING(50),
   content: db.TEXT,
-  tag_ids: db.STRING(50),
-  catalog_id: db.STRING(50),
+  overview: db.TEXT,
+  tags: db.STRING(50),
+  catalog: db.STRING(50),
   views: db.INTEGER,
-  html: db.STRING(50),
 });
