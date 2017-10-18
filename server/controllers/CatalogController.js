75fa2eda805acb0094658c054f5a1041fccfb64e
wangyu <13022200357@163.com>
2017-10-18 14:41:29 +0800

    [后端]:增加分类功能接口

--- server/controllers/CatalogController.js
+++ server/controllers/CatalogController.js
@@ -5,14 +5,34 @@ router.get('/1.0/catalog', async (ctx) => {
   ctx.body = await CatalogService.findAll();
 });
 
+router.get('/1.0/catalog/paging', async (ctx) => {
+  const req = ctx.query;
+  ctx.body = await CatalogService.findAllByPaging(req);
+});
+
 router.post('/1.0/catalog/add', async (ctx) => {
   const req = ctx.request.body;
-  ctx.body = await CatalogService.add(req.catalog);
+  ctx.body = await CatalogService.add(req);
 });
 
 router.put('/1.0/catalog/update', async (ctx) => {
   const req = ctx.request.body;
-  ctx.body = await CatalogService.add(req);
+  ctx.body = await CatalogService.update(req);
+});
+
+router.delete('/1.0/catalog/delete', async (ctx) => {
+  const req = ctx.query;
+  ctx.body = await CatalogService.delete(req.id);
+});
+
+router.delete('/1.0/catalog/batchDelete', async (ctx) => {
+  const req = ctx.query;
+  ctx.body = await CatalogService.batchDelete(req.ids);
+});
+
+router.post('/1.0/catalog/search', async (ctx) => {
+  const req = ctx.request.body;
+  ctx.body = await CatalogService.search(req);
 });
 
 module.exports = router;
--- server/services/CatalogService.js
+++ server/services/CatalogService.js
@@ -2,6 +2,7 @@ const Catalog = require('../models/Catalog');
 const ReturnData = require('../util/ReturnData');
 const ErrorMessage = require('../constants/ErrorMessage');
 const log4js = require('koa-log4');
+const Sequelize = require('sequelize');
 
 const logger = log4js.getLogger('app');
 
@@ -20,12 +21,33 @@ CatalogService.findAll = async () => {
 };
 
 /**
+ * 分页
+ */
+CatalogService.findAllByPaging = async ({ pageSize, pageIndex }) => {
+  try {
+    const data = await Catalog.findAndCountAll({
+      limit: parseInt(pageSize, 0),
+      offset: (pageIndex - 1) * parseInt(pageSize, 0),
+    });
+    return ReturnData.success(data);
+  } catch (err) {
+    logger.error(err.stack);
+    return ReturnData.error(ErrorMessage.NETWORK_ERROR);
+  }
+};
+
+/**
  * 增加分类
  */
 CatalogService.add = async (catalog) => {
   try {
     const data = await Catalog.create(catalog);
-    return ReturnData.success(data);
+    // 添加成功会返回该条数据，所以根据id去判断是否成功
+    if (data.id) {
+      return ReturnData.success(data);
+    } else {
+      return ReturnData.error(ErrorMessage.ADD_ERROR);
+    }
   } catch (err) {
     logger.error(err.stack);
     return ReturnData.error(ErrorMessage.NETWORK_ERROR);
@@ -75,8 +97,107 @@ CatalogService.update = async (obj) => {
       where: {
         id: obj.id,
       },
+      individualHooks: true,
     });
-    return ReturnData.success(data);
+    /**
+     * 更新成功后返回一个数组
+     * 数组第一个是affectedCount
+     * 数组第二个是affectedRows
+     * 首先根据是不是数组，然后判断affectedCount是不是大于0
+     */
+    if (data && data instanceof Array && data[0] > 0) {
+      return ReturnData.success(data);
+    } else {
+      return ReturnData.error(ErrorMessage.UPDATE_ERROR);
+    }
+  } catch (err) {
+    logger.error(err.stack);
+    return ReturnData.error(ErrorMessage.NETWORK_ERROR);
+  }
+};
+
+/**
+ * delete
+ */
+CatalogService.delete = async (id) => {
+  try {
+    const data = await Catalog.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (data > 0) {
+      return ReturnData.success(data);
+    } else {
+      return ReturnData.error(ErrorMessage.DELETE_ERROR);
+    }
+  } catch (err) {
+    logger.error(err.stack);
+    return ReturnData.error(ErrorMessage.NETWORK_ERROR);
+  }
+};
+
+/**
+ * batchDelete
+ */
+CatalogService.batchDelete = async (ids) => {
+  try {
+    ids = ids.split(',');
+    for (let i = 0; i < ids.length; i += 1) {
+      const data = await Catalog.destroy({
+        where: {
+          id: ids[i],
+        },
+      });
+      if (data < 1) {
+        return ReturnData.error(ErrorMessage.BATCH_DELETE_ERROR);
+      }
+    }
+    return ReturnData.success();
+  } catch (err) {
+    logger.error(err.stack);
+    return ReturnData.error(ErrorMessage.NETWORK_ERROR);
+  }
+};
+
+/**
+ * search
+ */
+CatalogService.search = async (req) => {
+  const {
+    or,
+    and,
+    gt,
+    lt,
+    like,
+  } = Sequelize.Op;
+  const { pageSize, pageIndex, params } = req;
+
+  try {
+    const result = await Catalog.findAndCountAll({
+      where: {
+        [or]: [
+          {
+            name: {
+              [like]: `%${params.name}%`,
+            },
+          },
+          {
+            [and]: [
+              {
+                updatedAt: { [gt]: params.start ? parseInt(params.start, 0) : 0 },
+              },
+              {
+                updatedAt: { [lt]: params.end ? parseInt(params.end, 0) : 0 },
+              },
+            ],
+          },
+        ],
+      },
+      limit: parseInt(pageSize, 0),
+      offset: (pageIndex - 1) * parseInt(pageSize, 0),
+    });
+    return ReturnData.success(result);
   } catch (err) {
     logger.error(err.stack);
     return ReturnData.error(ErrorMessage.NETWORK_ERROR);
@@ -114,6 +235,5 @@ CatalogService.isExit = async (name) => {
     logger.error(err.stack);
   }
 };
-
 
 module.exports = CatalogService;
