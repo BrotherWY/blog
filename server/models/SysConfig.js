a58b0d254abfb2c2ca1450f455ed78b85da685c1
wangyu <13022200357@163.com>
2017-10-25 12:00:01 +0800

    [后端]:更改sys_config字段

--- server/models/SysConfig.js
+++ server/models/SysConfig.js
@@ -3,7 +3,8 @@ const db = require('../db/db');
 module.exports = db.defineModel('sys_config', {
   name: db.STRING(20),
   url: db.STRING(50),
-  icon: db.STRING(50),
+  img_url: db.STRING(200),
   code: db.STRING(20),
   sort: db.INTEGER,
+  content: db.TEXT,
 });
