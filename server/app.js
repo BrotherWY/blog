c8b556816061b74412f7437ae0f2addf02664524
wangyu <13022200357@163.com>
2017-10-30 13:39:57 +0800

    [后端]:update 引入controller方式

--- server/app.js
+++ server/app.js
@@ -6,7 +6,16 @@ const serve = require('koa-static');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser');
 const log4js = require('./util/InitLog');
-const controllers = require('./controllers');
+
+const ArticleController = require('./controllers/ArticleController');
+const CatalogController = require('./controllers/CatalogController');
+const IndexController = require('./controllers/IndexController');
+const LoginController = require('./controllers/LoginController');
+const MenuController = require('./controllers/MenuController');
+const SysConfigController = require('./controllers/SysConfigController');
+const TagController = require('./controllers/TagController');
+const UploadController = require('./controllers/UploadController');
+
 
 const app = new Koa();
 // error handler
@@ -20,11 +29,15 @@ app.use(bodyparser({
 app.use(json());
 app.use(cors());
 app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }));
-
 // controller
-for (const i in controllers) {
-  app.use(controllers[i].routes(), controllers[i].allowedMethods());
-}
+app.use(UploadController.routes(), UploadController.allowedMethods());
+app.use(TagController.routes(), TagController.allowedMethods());
+app.use(SysConfigController.routes(), SysConfigController.allowedMethods());
+app.use(MenuController.routes(), MenuController.allowedMethods());
+app.use(LoginController.routes(), LoginController.allowedMethods());
+app.use(IndexController.routes(), IndexController.allowedMethods());
+app.use(CatalogController.routes(), CatalogController.allowedMethods());
+app.use(ArticleController.routes(), ArticleController.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
