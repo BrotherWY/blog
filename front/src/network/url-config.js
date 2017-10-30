de760ad740e636227f2c4e7c141dc34aef6fb115
wangyu <13022200357@163.com>
2017-10-30 16:02:23 +0800

    [前端]-前台:更改production地址

--- front/src/network/url-config.js
+++ front/src/network/url-config.js
@@ -1,4 +1,4 @@
 const UrlConfig = {};
 if (process.env.NODE_ENV === 'development') UrlConfig.apiUrl = 'http://localhost:3000';
-else UrlConfig.apiUrl = 'https://easy-mock.com/mock/59ca4b15e0dc663341bbffa1/1.0';
+else UrlConfig.apiUrl = 'http://104.129.182.249:3000';
 module.exports = UrlConfig;
