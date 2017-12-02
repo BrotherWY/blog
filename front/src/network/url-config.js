9f6b86d6562cc664ae48c3cf0a57bcf61b41a642
brotherwang <13022200357@163.com>
2017-12-02 11:35:38 +0800

    [前端-前台]:更改服务器地址

--- front/src/network/url-config.js
+++ front/src/network/url-config.js
@@ -1,4 +1,4 @@
 const UrlConfig = {};
 if (process.env.NODE_ENV === 'development') UrlConfig.apiUrl = 'http://localhost:3000';
-else UrlConfig.apiUrl = 'http://104.129.182.249:3000';
+else UrlConfig.apiUrl = 'http://207.246.102.22:3000';
 module.exports = UrlConfig;
